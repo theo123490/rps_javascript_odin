dcaf80e243d8c60f21cae7dbeaed60da048f0fa1
Theodore Gautama Chandra <theoengphys@gmail.com>
2022-08-05 22:35:33 +0700

    add getComputerChoice

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ function playRound(playerSelection, computerSelection) {
   // your code here!
 }
 
+function getComputerChoice() {
+  rng_result=Math.floor( Math.random() * 100 % 3)
+  if(rng_result == 0) {
+    return "rock"
+  } else if (rng_result == 1) {
+    return "paper"
+  } else if (rng_result == 2) {
+    return "scissor"
+  }
+}
+
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
